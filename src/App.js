5a71d8ed72eeee1ae4fa90331e0daf046fbe3fde
springheart199279 <springheart199279@gmail.com>
2021-11-04 00:02:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
 import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
-
+import './App.css';
 
 const App = () => {
   const [courseGoals, setCourseGoals] = useState([
